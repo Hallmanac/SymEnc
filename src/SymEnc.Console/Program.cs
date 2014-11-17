9cf20a81d50afd695367c75425da9cc465ba88cc
Hallmanac <Brian@Hallmanac.com>
2014-11-17 08:50:19 -0500

    Changed the name of the console project to remove the namespace conflict. Also added nuget packages to the .gitignore.

--- src/SymEnc.Console/Program.cs
+++ src/SymEnc.Console/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using SymEnc.Core;
 
-namespace SymEnc.Console
+namespace SymEnc.ConsoleApp
 {
     internal class Program
     {
@@ -12,32 +12,34 @@ namespace SymEnc.Console
             while(continueGenerating256BitKeys)
             {
                 var keyFor256 = symEnc.Generate256BitKey();
-                System.Console.WriteLine("The generated 256 bit key is:\n{0}", keyFor256);
-                System.Console.WriteLine("\nWould you like to generate another 256 bit key? (Y or N)");
-                var continueAnswer = System.Console.ReadLine();
+                Console.WriteLine("The generated 256 bit key is:\n{0}", keyFor256);
+                Console.WriteLine("\nWould you like to generate another 256 bit key? (Y or N)");
+                var continueAnswer = Console.ReadLine();
                 continueGenerating256BitKeys = !string.IsNullOrEmpty(continueAnswer) &&
                                                (string.Equals("Y", continueAnswer, StringComparison.CurrentCultureIgnoreCase) ||
                                                 string.Equals("yes", continueAnswer, StringComparison.CurrentCultureIgnoreCase));
             }
             
-            System.Console.WriteLine("\nEnter an even numbered integer to generate a hexidecimal key based on that length...");
+            Console.WriteLine("\nEnter an even numbered integer to generate a hexidecimal key based on that length...");
 
-            var keySizeStr = System.Console.ReadLine();
+            var keySizeStr = Console.ReadLine();
             int keySize;
             if(Int32.TryParse(keySizeStr, out keySize))
             {
                 var generatedKey = symEnc.GenerateKeyFromByteLength(keySize);
-                System.Console.WriteLine("\nThe generated key based on a length of {0} is:\n{1}", keySizeStr, generatedKey);
+                Console.WriteLine("\nThe generated key based on a length of {0} is:\n{1}", keySizeStr, generatedKey);
             }
             else
             {
-                System.Console.WriteLine("\nThe value entered for a key was invalid.");
+                Console.WriteLine("\nThe value entered for a key was invalid.");
             }
-            System.Console.WriteLine("\nEnter a string to hash using the HMACSHA1 algorithm.");
-            var textToHash = System.Console.ReadLine();
+            Console.WriteLine("\nEnter a string to hash using the HMACSHA1 algorithm.");
+            var textToHash = Console.ReadLine();
             var hashedText = symEnc.ComputeHmacSha1ForHex(textToHash);
 
-            System.Console.WriteLine("\nThe computed hash is:\n{0}", hashedText);
+            Console.WriteLine("\nThe computed hash is:\n{0}", hashedText);
+
+            Console.ReadKey();
         }
     }
 }
